80d7a301e9576896a00a8c6a9dc608f5685d6b2d
Riddh Modi <riddhmodi2003@gmail.com>
2024-08-09 08:41:12 +0530

    add this project

---
+++ Netflix.js
@@ -0,0 +1,5 @@
+var btn = document.getElementById('sign_in');
+
+btn.addEventListener('click',function() {
+   window.location.href ="LoginPage.html"; 
+});
---
+++ README.md
@@ -0,0 +1,2 @@
+# Netflix
+I have hosted this website using GitHub. And I have used HTML/CSS and Javascript.
